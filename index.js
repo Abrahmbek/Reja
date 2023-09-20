4af0097bbd40e2f699e8ee84bc84012e07a30b02
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2023-09-20 21:23:09 +0900

    feat: 1

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("salom");
+//TASK C
+
+
+const  tekshir = (matn1, matn2) => {
+ 
+
+    const qosh1 = matn1.split('').sort();
+    
+    const qosh2 = matn2.split('').sort();
+  
+    
+    return qosh1.join('') === qosh2.join('');
+  }
+  
+  
+  console.log(tekshir("abababa", "abababa")); 
