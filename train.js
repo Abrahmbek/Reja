e24e5faf60e905f182bb19b1939144cc59f33d9c
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2023-09-21 09:38:07 +0900

    add: task d

--- train.js
+++ train.js
@@ -24,24 +24,24 @@
 //TASK B
 
 
-  function raqamQaytar(matn) {
-    let sana = 0;
+//   function raqamQaytar(matn) {
+//     let sana = 0;
   
-    for (let i = 0; i < matn.length; i++) {
-      const belgi = matn.charAt(i);          // bu yerda charAt  method dan foydalanib string ni ichidagi belgilani indexlani olishda ishlatiladi
+//     for (let i = 0; i < matn.length; i++) {
+//       const belgi = matn.charAt(i);          // bu yerda charAt  method dan foydalanib string ni ichidagi belgilani indexlani olishda ishlatiladi
      
-      if (belgi >= '0' && belgi <= '9') {          // if dan foydalanib matn ichida 0 dan 9gacha raqam bormi shu tekshiradi 
-        sana++;
-      }                                          // agar bolsa bitta qoshib qoyadi
-    }
+//       if (belgi >= '0' && belgi <= '9') {          // if dan foydalanib matn ichida 0 dan 9gacha raqam bormi shu tekshiradi 
+//         sana++;
+//       }                                          // agar bolsa bitta qoshib qoyadi
+//     }
   
-    return sana;
-  }
+//     return sana;
+//   }
   
  
-  const matn1 = "ag1aw5g6fhuwh2ofhwu0rh2roh28";            // bu yerda matn1 ozgaruvchisi orqali matnimizni kiritib oldik
-  const sana = raqamQaytar(matn1);                        // va uni sana degan opzgaruvchiga berib yubordik
-  console.log("matndagi raqamlar soni:", sana);              
+//   const matn1 = "ag1aw5g6fhuwh2ofhwu0rh2roh28";            // bu yerda matn1 ozgaruvchisi orqali matnimizni kiritib oldik
+//   const sana = raqamQaytar(matn1);                        // va uni sana degan opzgaruvchiga berib yubordik
+//   console.log("matndagi raqamlar soni:", sana);              
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -50,16 +50,74 @@
 //TASK C
 
 
-const  tekshir = (matn1, matn2) => {
+// const  tekshir = (matn1, matn2) => {
  
 
-    const qosh1 = matn1.split('').sort();
+//     const qosh1 = matn1.split('').sort();
     
-    const qosh2 = matn2.split('').sort();
+//     const qosh2 = matn2.split('').sort();
   
     
-    return qosh1.join('') === qosh2.join('');
-  }
+//     return qosh1.join('') === qosh2.join('');
+//   }
   
   
-  console.log(tekshir("abababa", "abababa")); 
+//   console.log(tekshir("abababa", "abababa")); 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// task d
+
+const moment = require("moment");
+class Shop {
+   
+     constructor(cola_bor, non_bor, pista_bor) {
+      this.mahsulotlar = {
+        cola: cola_bor,
+        non: non_bor,
+        pista: pista_bor
+      };
+     }
+
+
+     xabar(){
+      const time = moment().format("HH:MM:SS");
+      console.log(`[${time}] eslatma mahsulot:,`, this.mahsulotlar);
+     }
+
+     sotuv(mahsulot, mavjud){
+      const time = moment().format("HH:MM:SS");
+      if(this.mahsulotlar[mahsulot]) {
+        this.mahsulotlar[mahsulot] -= mavjud;
+        console.log(`[${time}] da ushbu ${mavjud} mahsulot sotildi ${this.mahsulot}`);
+        }
+        else{
+          console.log(`[${time}] da ushbu ${mahsulot} mavjud emas`);
+        }
+    
+     }
+      
+     qabul(mahsulot, mavjud) {
+      const time = moment().format("HH:MM:SS");
+      if(this.mahsulotlar[mahsulot]) {
+        this.mahsulotlar[mahsulot] += mavjud;
+        console.log(`[${time}] da ushbu ${mavjud} mahsulot ${mahsulot} qoshildi`);
+        }
+        else{
+          console.log(`[${time}] da ushbu ${mahsulot} qabul qilinmadi`);
+        }
+    
+     }
+     qoldiq(){
+      const time = moment().format("HH:MM:SS");
+      console.log(`[${time}] da mahsulotlar mavjud:`, this.mahsulotlar);
+     }
+
+
+}
+const shop = new Shop(4, 3, 1);
+
+shop.xabar();
+shop.sotuv('choy', 3);
+shop.qabul('pista', 4);
+shop.qoldiq();
