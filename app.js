3b17cb8393a31ca2da6df1bd38097b76e215f1a0
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2023-10-04 22:18:49 +0900

    feat update and delete

--- app.js
+++ app.js
@@ -1,46 +1,88 @@
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
+const express = require("express");                      //express ni chaqirib olyapmiz      
+
+const app = express();                             // app ga express ni tenglab olyapmiz
 
 
 
 // MongoDB call
+ 
+ const db = require("./server").db();           // mongo db ni chaqirib olyapmiz  client ni ichida db ni olyapmiz   
 
- const db = require("./server").db();
+const mongodb = require("mongodb");          // newmongodb ichida id bor oshaning uchun bia qaytadan require qilib oldik
+
 
 //1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));                  //  routing ichidagi public file ni serverga ulanga klient larga ochib beryapmiz
+app.use(express.json());                       // kirib kelayotgan malumotlarni express json formatdan object formatiga  aylantirib olyapmiz
+app.use(express.urlencoded({extended: true}));   // traditional frontend nni post qilib otkazib yuboryapti
 
 // 2: Session code
 // 3: Views code
 
- app.set("views",   "views");
- app.set("view engine",  "ejs",);
+ app.set("views",   "views");                     // frontend file larni viewa orqali topasan deyapmiz 
+ app.set("view engine",  "ejs",);   //frontend ni ejs orqali qurishni aytyapmiz
 
 
 // 4 Routing code
- app.post("/create-item", (req, res) => {
+ 
+app.post("/create-item", (req, res) => {          // bu qismlar end poit deb ataladi  //form ni ichidagi create -item 
      console.log("user entered  /create-item")
- const new_reja = req.body.reja;
- db.collection("plans").insertOne({ reja: new_reja}, (err, data) => {
+ const new_reja = req.body.reja;                      // browser.js ni  html ni ichida kelyapti 
+ db.collection("plans").insertOne({ reja: new_reja}, (err, data) => {   // va db ga borib 
     console.log(data.ops);
     res.json(data.ops[0]);
  });
+}); 
+
+// delete session
+app.post("/delete-item", (req, res) =>{
+    const id = req.body.id;               // bu yerda id orqali qiymatni qabul qilib olyapti
+   db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id) }, function(err, data){
+    res.json ({state: "success"});
+   }
+   );     
+   
+});
+  
+
+
+   //update qismi
+   
+app.post("/edit-item", (req, res) =>{
+    const data = req.body;
+    console.log(data);
+    db.collection("plans").findOneAndUpdate(
+        {_id: new mongodb.ObjectId(data.id)},
+     {$set: {reja: data.new_input} },
+    function(err, data) {
+        res.json({state: "success"});
+    });
+   
 });
 
- app.get ("/", function (req, res) {
- 
-     db.collection("plans")
-       .find()
-       .toArray((err, data) => {
-       if(err) {
+ // delete-all session
+ app.post("/delete-all", (req, res) => {
+    if(req.body.delete_all) {
+        db.collection("plans").deleteMany(function () {
+            res.json({state: "hamma rejalar ochirildi"});
+        });
+    }
+   });
+
+
+/// res.json   res.send   res.end    // res.render
+
+ app.get ("/", function (req, res) {            //general path  ga kirganda function ishga tushadi     
+                       
+     db.collection("plans")      //mongo db ni collection plans ni ichidagi static method 
+       .find()                      // hamm narsani top izlayapti
+       .toArray((err, data) => {          // cva bizga arry qilib malumotlsarni qaytarib beryapti
+       if(err) {                         // agar bizga error bolsa uni kirsat 
            console.log(err);
            res.end("something went wrong");
-       } else {
-           res.render("reja", { items: data});
-       }
+       } else {                                    // aks holda itmes ichidagi datalarni olib ber deyapmiz
+           res.render("reja", { items: data});      // ejs orqali javob beryapmiz data ni ham birga yuboryapmiz     
+          }
     });
 });
 
--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,5 @@
+const { response } = require("../app");
+
 console.log("fronted qoshildi");
 function itemTemplate(item) {
 return ` <li 
@@ -14,15 +16,15 @@ class="list-group-item list-group-item-info d-flex align-items-center justify-co
 </li>`;
 }
 
-let createField = document.getElementById("create-field");
+let createField = document.getElementById("create-field");  // bu yerda creat feild ni ushlab olyapmiz htmlda 
 
 document
 .getElementById("create-form")
 .addEventListener("submit", function (e) {
 e.preventDefault();
 
-    axios
-    .post("/create-item", { reja: createField.value })
+    axios      // modern request
+    .post("/create-item", { reja: createField.value })   // create -field ni qolga ovoldik
     .then((response) =>{
         document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data));
         createField.value = "";
@@ -32,3 +34,66 @@ e.preventDefault();
         console.log("qaytadan xarakat qiling");
     });
 });
+
+
+document.addEventListener("click", function(e){
+      //delete oper
+      console.log(e.target);
+    if(e.target.classList.contains("delete-me")) {    // ejs da classlist contains ichidan delete me button ni ushlab oldik
+      
+      
+        if(confirm('are you sure?')) {                //confirm browserga tegishli js bilan birga ishlaydi
+            axios
+            .post("/delete-item", {id: e.target.getAttribute("data-id")})
+            .then((response) => {
+                console.log(response.data);
+                e.target.parentElement.parentElement.remove();
+            })
+            .catch((err) =>{
+                console.log("qaytadan xarakat qiling");
+            });
+        }                                             
+        
+       
+    }
+// edit qismi
+   
+   if(e.target.classList.contains("edit-me")) {
+       let userInput = prompt("ozgartirish kirirting",
+        e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+       );
+       if (userInput) {
+        axios
+        .post("/edit-item", {
+            id: e.target.getAttribute("data-id"),
+            new_input: userInput,
+            })
+            .then((response )=>{
+            console.log(response.data);
+            e.target.parentElement.parentElement.querySelector(
+                ".item-text"
+                ).innerHTML = userInput;
+
+        })
+        .catch((err) =>{
+          console.log("qaytadan xarakat qiling");
+        });
+       }
+    }
+});
+    // hammasini ochirish 
+
+  document.getElementById("clean-all").addEventListener("click", function() {
+    axios
+    .post("/delete-all", {delete_all: true })
+    .then((response) => {
+        alert(response.data.state);
+        document.location.reload();
+    });
+  });
+
+
+
+
+
+
